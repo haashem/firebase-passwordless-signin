eafa8fd2aa254c30502cb445d082228a9b0d442a
Hashem <hashemp206@yahoo.com>
2023-05-18 12:51:46 +0100

    Add check email link to listen to sign-in link

--- lib/auth/passwordless_authenticator.dart
+++ lib/auth/passwordless_authenticator.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:firebase_auth/firebase_auth.dart' as firebase_auth;
+import 'package:firebase_dynamic_links/firebase_dynamic_links.dart';
 import 'package:fpdart/fpdart.dart';
 import 'package:passwordless_signin/auth/email_secure_store.dart';
 import 'package:passwordless_signin/auth/models/failure.dart';
@@ -8,11 +9,14 @@ import 'package:passwordless_signin/auth/models/sign_in_link_settings.dart';
 
 class PasswordlessAuthenticator {
   final firebase_auth.FirebaseAuth _firebaseAuth;
+  final FirebaseDynamicLinks _firebaseDynamicLinks;
   final EmailSecureStore _emailSecureStore;
   final SignInLinkSettings _signinLinkSettings;
+  void Function(EmailLinkFailure value)? onSigninFailure;
 
   PasswordlessAuthenticator(
     this._firebaseAuth,
+    this._firebaseDynamicLinks,
     this._emailSecureStore,
     this._signinLinkSettings,
   );
@@ -67,5 +71,33 @@ class PasswordlessAuthenticator {
     } catch (e) {
       return left(EmailLinkFailure.signInFailed(e.toString()));
     }
+  }
+
+  /// Sets up listeners to process all links from [FirebaseDynamicLinks.instance.getInitialLink()] and [FirebaseDynamicLinks.instance.onLink]
+  Future<void> checkEmailLink() async {
+    // Listen to incoming links when the app is open
+    _firebaseDynamicLinks.onLink.listen(
+      (event) async {
+        final result = await signInWithEmailLink(event.link);
+        result.fold((failure) => onSigninFailure?.call(failure), (_) => null);
+      },
+      onError: _handleLinkError,
+    );
+
+    try {
+      // Check dynamic link once on app startup.
+      // This is required to process any dynamic links that are opened when the app was closed
+      final linkData = await _firebaseDynamicLinks.getInitialLink();
+      final link = linkData?.link.toString();
+      if (link != null) {
+        signInWithEmailLink(linkData!.link);
+      }
+    } catch (e) {
+      _handleLinkError(e);
+    }
+  }
+
+  void _handleLinkError(Object error) {
+    onSigninFailure?.call(EmailLinkFailure.linkError(error.toString()));
   }
 }
