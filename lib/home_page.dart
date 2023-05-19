8a67f02ed27dc5e7d3f249892c2e592fb2eabad7
Hashem <hashemp206@yahoo.com>
2023-05-19 16:33:19 +0100

    Add sign out

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,28 +1,36 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+  final VoidCallback onSignoutRequest;
+  const HomePage({super.key, required this.onSignoutRequest});
 
   @override
   Widget build(BuildContext context) {
-    return Material(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const SizedBox(
-            width: 220,
-            height: 220,
-            child: Image(image: AssetImage('assets/images/diamond.png')),
-          ),
-          Text(
-            'Welcome!',
-            style: Theme.of(context)
-                .textTheme
-                .headlineSmall
-                ?.copyWith(fontWeight: FontWeight.bold),
-            textAlign: TextAlign.center,
-          ),
-        ],
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        actions: [TextButton(onPressed: onSignoutRequest, child: const Text('Signout'))],
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const SizedBox(
+              width: 220,
+              height: 220,
+              child: Image(image: AssetImage('assets/images/diamond.png')),
+            ),
+            Text(
+              'Welcome!',
+              style: Theme.of(context)
+                  .textTheme
+                  .headlineSmall
+                  ?.copyWith(fontWeight: FontWeight.bold),
+              textAlign: TextAlign.center,
+            ),
+          ],
+        ),
       ),
     );
   }
