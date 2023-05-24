cfab05c10c57174fc641d021497afa1a045a5981
Hashem <hashemp206@yahoo.com>
2023-05-24 20:18:23 +0100

    Add tests for PasswordlessAuthenticator

--- lib/auth/models/user.dart
+++ lib/auth/models/user.dart
@@ -1,7 +1,19 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 class User {
   final String id;
   factory User(String id) {
     return User._(id);
   }
   User._(this.id);
+
+  @override
+  bool operator ==(covariant User other) {
+    if (identical(this, other)) return true;
+  
+    return 
+      other.id == id;
+  }
+
+  @override
+  int get hashCode => id.hashCode;
 }
